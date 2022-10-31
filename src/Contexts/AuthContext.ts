a8d5d4637c6cd83339c8862693080d8b6a147c09
Jan Kočvara <kimichisxd285@gmail.com>
2022-10-31 02:22:07 +0100

    export `IAuthContext`

--- src/Contexts/AuthContext.ts
+++ src/Contexts/AuthContext.ts
@@ -1,11 +1,11 @@
 import { createContext } from 'react';
 import { IPerson } from '../types';
 
-interface IAuthContext {
+export interface IAuthContext {
   user: IPerson,
   token: string,
   setToken: (token: string) => void,
-  setUser: (user: IPerson) => void, 
+  setUser: (user: IPerson) => void,
 }
 
 export const AuthContext = createContext<IAuthContext>({
@@ -16,6 +16,5 @@ export const AuthContext = createContext<IAuthContext>({
   // eslint-disable-next-line @typescript-eslint/no-empty-function
   setUser: () => {},
 });
-  
+
 export const AuthContextProvider = AuthContext.Provider;
-  
