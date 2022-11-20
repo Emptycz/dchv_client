57fa01e124b6ed929f777ef776593881c04b3911
Jan Kočvara <kimichisxd285@gmail.com>
2022-11-20 01:32:23 +0100

    introduce `useAuth` hook

---
+++ src/Hooks/Auth.hooks.ts
@@ -0,0 +1,32 @@
+import { IPerson } from './../types';
+import { AuthContext } from './../Contexts/AuthContext';
+import { useContext } from 'react';
+
+type AuthType = {
+  user?: IPerson,
+  signIn: (user: IPerson, token: string) => void,
+  signOut: () => void,
+};
+
+const useAuth = (): AuthType => {
+  const { user, setToken, setUser } = useContext(AuthContext);
+
+  const signIn = (user: IPerson, token: string) => {
+    setUser(user);
+    setToken(token);
+  };
+
+  const signOut = () => {
+    setUser();
+    setToken();
+  };
+
+
+  return {
+    user,
+    signIn,
+    signOut,
+  };
+};
+
+export default useAuth;
