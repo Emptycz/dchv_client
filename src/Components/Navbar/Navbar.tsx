075de2f1f916799c6be92aadcb045d471d0d2068
Jan Kočvara <kimichisxd285@gmail.com>
2022-11-20 01:34:32 +0100

    use new `useAuth` hook

--- src/Components/Navbar/Navbar.tsx
+++ src/Components/Navbar/Navbar.tsx
@@ -2,18 +2,29 @@ import { DarkMode, LightMode } from '@mui/icons-material';
 import { Avatar } from '@mui/material';
 import { deepOrange } from '@mui/material/colors';
 import React, { useContext } from 'react';
-import { AuthContext, IAuthContext } from '../../Contexts/AuthContext';
+import { useNavigate } from 'react-router-dom';
 import { ColorModeContext, IColorModeContext } from '../../Contexts/ColorModeContext';
+import useAuth from '../../Hooks/Auth.hooks';
 
 const Navbar = () => {
-  const { user }: IAuthContext = useContext(AuthContext);
   const { darkMode, setDarkMode }: IColorModeContext = useContext(ColorModeContext);
+
+  const navigate = useNavigate();
+  const { user, signOut } = useAuth();
+
+  const onSignOut = () => {
+    signOut();
+    navigate('/');
+  };
 
   return (
     <nav className='flex flex-row-reverse items-center h-16 bg-red-200 w-100 gap-6 text-gray-800 dark:bg-gray-900 dark:text-slate-200'>
       <div className='flex flex-row items-center gap-4 px-8'>
-        <span className='font-bold'> {user.firstname} {user.lastname} </span>
-        <Avatar sx={{ bgcolor: deepOrange[500] }}> {user.firstname?.[0]}{user.lastname?.[0]} </Avatar>
+        <span onClick={onSignOut}> Logout </span>
+      </div>
+      <div className='flex flex-row items-center gap-4 px-8'>
+        <span className='font-bold'> {user?.firstname} {user?.lastname} </span>
+        <Avatar sx={{ bgcolor: deepOrange[500] }}> {user?.firstname?.[0]}{user?.lastname?.[0]} </Avatar>
       </div>
       <ul className='flex flex-row gap-5'>
         <li> Notifications </li>
