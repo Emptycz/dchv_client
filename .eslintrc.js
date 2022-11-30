87e662ecced58654ff5baf60d22122f41da6c572
Jan Kočvara <kimichisxd285@gmail.com>
2022-11-30 14:31:11 +0100

    update `eslint` rules

---
+++ .eslintrc.js
@@ -0,0 +1,53 @@
+module.exports = {
+  ignorePatterns: ['!**/*', 'node_modules', 'src/types/informed/*'],
+  settings: {
+    'import/resolver': {
+      node: {
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
+        moduleDirectory: ['node_modules', 'src'],
+      },
+    },
+  },
+  'env': {
+    'browser': true,
+    'es2021': true,
+    'node': true
+  },
+  'extends': [
+    'eslint:recommended',
+    'plugin:react/recommended',
+    'plugin:@typescript-eslint/recommended'
+  ],
+  'parser': '@typescript-eslint/parser',
+  'parserOptions': {
+    'ecmaFeatures': {
+      'jsx': true
+    },
+    'ecmaVersion': 'latest',
+    'sourceType': 'module'
+  },
+  'plugins': [
+    'react',
+    '@typescript-eslint'
+  ],
+  'rules': {
+    'indent': [
+      'error',
+      2,
+      { 'SwitchCase': 1 }
+    ],
+    'linebreak-style': [
+      'warn',
+      'windows'
+    ],
+    'quotes': [
+      'error',
+      'single'
+    ],
+    'semi': [
+      'error',
+      'always'
+    ],
+    '@typescript-eslint/no-empty-function': 'off'
+  }
+};
