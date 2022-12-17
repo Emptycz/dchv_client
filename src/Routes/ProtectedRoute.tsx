04efa849f45fe90595db407d6f8629725a1f4b98
Jan Kočvara <kimichisxd285@gmail.com>
2022-12-17 09:14:32 +0100

    check for `role` in `ProtectedRoute`

--- src/Routes/ProtectedRoute.tsx
+++ src/Routes/ProtectedRoute.tsx
@@ -1,13 +1,18 @@
 import React from 'react';
 import { Navigate } from 'react-router-dom';
 import useAuth from '../Hooks/Auth.hook';
+import { IRole } from '../types';
 
 type ProtectedRouteType = {
+  role?: IRole;
   children: React.ReactElement | React.ReactElement[];
 }
 
-const ProtectedRoute: React.FC<ProtectedRouteType> = ({ children }) => {
+const ProtectedRoute: React.FC<ProtectedRouteType> = ({ role, children }) => {
   const { user } = useAuth();
+
+  // FIXME: We want to check if two arrays have at least 1 role in common
+  if (role && !user?.roles?.some((x) => x.name === role.name)) return (<Navigate to="/" />);
 
   if (!user || !user.id) return (<Navigate to="/" />);
   return (
